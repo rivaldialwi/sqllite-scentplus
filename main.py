6b5ba88f013b83994b93d722588287300781c3ba
unknown <rivaldialwi.com>
2024-06-13 17:19:35 +0700

    pusing

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 import streamlit as st
 import app
 import laporan
+import login
+import manage_accounts
 
-PAGES = {
+ADMIN_PAGES = {
     "Prediksi Sentimen": app,
-    "Laporan Analisis Sentimen": laporan
+    "Laporan Analisis Sentimen": laporan,
+    "Mengelola Akun": manage_accounts
 }
 
-st.sidebar.title('Menu')
-selection = st.sidebar.radio("Silahkan Memilih Menu", list(PAGES.keys()))
+USER_PAGES = {
+    "Prediksi Sentimen": app,
+    "Laporan Analisis Sentimen": laporan,
+}
 
-page = PAGES[selection]
-page.run()
+if 'logged_in' not in st.session_state:
+    st.session_state['logged_in'] = False
+
+if 'role' not in st.session_state:
+    st.session_state['role'] = None
+
+if not st.session_state['logged_in']:
+    login.login()
+else:
+    role = st.session_state['role']
+    
+    if role == 'admin':
+        PAGES = ADMIN_PAGES
+    elif role == 'user':
+        PAGES = USER_PAGES
+    
+    st.sidebar.title('Menu')
+    selection = st.sidebar.radio("Silahkan Memilih Menu", list(PAGES.keys()))
+    page = PAGES[selection]
+    page.run()

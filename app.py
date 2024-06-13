63b4cb74782a4d5bfc0c491f0f2dea667bca8aa2
unknown <rivaldialwi.com>
2024-06-13 12:24:49 +0700

    bismillah ga bentrok

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
 from datetime import datetime
+from io import BytesIO
 
 # Membaca model yang sudah dilatih
 logreg_model = joblib.load("model100.pkl")
@@ -54,42 +55,87 @@ def fetch_data():
     conn.close()
     return rows
 
+# Fungsi untuk mengonversi DataFrame ke Excel
+@st.cache_data
+def convert_df_to_excel(df):
+    output = BytesIO()
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=False, sheet_name='Sheet1')
+        writer.close()  # Gunakan writer.close() untuk menyimpan file
+    processed_data = output.getvalue()
+    return processed_data
+
 # Fungsi untuk menjalankan aplikasi
 def run():
-    st.title("Aplikasi Analisis Sentimen scentplussss")
-    input_text = st.text_input("Masukkan kalimat untuk analisis sentimen:")
-    
-    if 'data' not in st.session_state:
-        st.session_state['data'] = fetch_data()
+    st.title("Aplikasi Analisis Sentimen Moris")
 
-    if st.button("Analisis"):
-        if input_text.strip() == "":
-            st.error("Tolong masukkan sentimen terlebih dahulu.")
-        else:
-            result = classify_text(input_text)
-            st.write("Hasil Analisis Sentimen:", result)
-            insert_to_db(input_text, result)
-            st.session_state['data'] = fetch_data()
+    tab1, tab2 = st.tabs(["Prediksi Satu Kalimat", "Prediksi File"])
+
+    with tab1:
+        st.header("Masukkan kalimat untuk analisis sentimen:")
+        input_text = st.text_input("Masukkan kalimat")
     
-    # Menampilkan data dari session state sebagai tabel
-    data = st.session_state['data']
-    if data:
-        df = pd.DataFrame(data, columns=['id', 'text', 'sentiment', 'date'])
-        
-        # Tambahkan CSS untuk menyesuaikan header
-        hide_dataframe_row_index = """
-            <style>
-            thead th {
-                text-align: center;
-            }
-            </style>
-        """
-        st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
-        
-        # Tampilkan tabel tanpa indeks
-        st.write(df.to_html(index=False, escape=False), unsafe_allow_html=True)
-    else:
-        st.write("Tidak ada data yang tersedia.")
+        if 'data' not in st.session_state:
+            st.session_state['data'] = fetch_data()
+
+        if st.button("Analisis"):
+            if input_text.strip() == "":
+                st.error("Tolong masukkan kalimat terlebih dahulu.")
+            else:
+                result = classify_text(input_text)
+                st.write("Hasil Analisis Sentimen:", result)
+                insert_to_db(input_text, result)
+                st.session_state['data'] = fetch_data()
+    
+        # Menampilkan data dari database sebagai tabel
+        data = st.session_state['data']
+        if data:
+            df = pd.DataFrame(data, columns=['id', 'text', 'sentiment', 'date'])
+            
+            # Tambahkan CSS untuk menyesuaikan header
+            hide_dataframe_row_index = """
+                <style>
+                thead th {
+                    text-align: center;
+                }
+                </style>
+            """
+            st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
+            
+            # Tampilkan tabel tanpa indeks
+            st.write(df.to_html(index=False, escape=False), unsafe_allow_html=True)
+        else:
+            st.write("Tidak ada data yang tersedia.")
+
+    with tab2:
+        st.header("Unggah file untuk Prediksi Sentimen")
+        uploaded_file = st.file_uploader("Unggah file Excel", type=["xlsx"], key="file_uploader")
+
+        if uploaded_file is not None:
+            # Baca file Excel
+            df = pd.read_excel(uploaded_file)
+            
+            # Periksa apakah kolom 'Text' ada di file yang diunggah
+            if 'Text' in df.columns:
+                # Inisialisasi TF-IDF Vectorizer dan fit_transform pada data teks
+                X = df['Text'].apply(clean_text)
+                X_tfidf = tfidf_vectorizer.transform(X)
+                
+                # Lakukan prediksi
+                df['Human'] = logreg_model.predict(X_tfidf)
+                
+                # Tampilkan prediksi
+                st.write(df)
+                
+                # Buat tombol unduh
+                st.download_button(
+                    label="Unduh file dengan prediksi",
+                    data=convert_df_to_excel(df),
+                    file_name="prediksi_sentimen.xlsx",
+                    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                )
+            else:
+                st.error("File harus memiliki kolom 'Text'.")
 
 if __name__ == "__main__":
     run()
